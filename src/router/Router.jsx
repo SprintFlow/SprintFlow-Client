12c35adccf0bc7991d4ac5f34f49f97cbf00be83
guiss26 <guissella584@gmail.com>
2025-10-21 15:16:01 -0700

    refactor: comment out routes without exported elements. Relates to #7

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,15 +3,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 
 import App from "../App"; 
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+// import Login from "../pages/Login";
+// import Register from "../pages/Register";
 import Results from "../pages/Results";
-import AdminProfile from "../pages/AdminProfile";
-import UserProfile from "../pages/UserProfile";
+// import AdminProfile from "../pages/AdminProfile";
+// import UserProfile from "../pages/UserProfile";
 import UserDashboard from "../pages/UserDashboard";
 
-const routerSprint = createBrowserRouter([
-  {
+const routerSprint = createBrowserRouter([{
     path: "/",
     element: <Layout />,
     children: [
@@ -20,35 +19,22 @@ const routerSprint = createBrowserRouter([
         element: <App />,
       },
       {
-        path: "user-dashboard",
+        path: "/user-dashboard",
         element: <UserDashboard />,
       },
       {
-        path: "results",
+        path: "/results",
         element: <Results />,
       },
-      {
-        path: "admin-profile",
-        element: <AdminProfile />,
-      },
-      {
-        path: "user-profile",
-        element: <UserProfile />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "*",
-    element: <h1>404 - Page not found</h1>,
-  },
-]);
+      // {
+      //   path: "admin-profile",
+      //   element: <AdminProfile />,
+      // },
+      // {
+      //   path: "user-profile",
+      //   element: <UserProfile />,
+      // }
+    ]
+}]);
 
 export default routerSprint;
