60beda7774ce1ba4dbff8380fb21116bb50aca5f
Aday25 <aday.alvarez84@gmail.com>
2025-10-29 00:23:03 +0100

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 import routerSprint from "./router/Router";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#3f51b5',
+      main: "#3f51b5",
     },
     secondary: {
-      main: '#f50057',
+      main: "#f50057",
     },
   },
 });
